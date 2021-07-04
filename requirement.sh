8ead68a41e614365719a0cfdcf30517c51e214de
STRIX.D <70028787+Strix2109@users.noreply.github.com>
2021-07-05 00:07:20 +0530

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -13,7 +13,7 @@ apt install ruby -y
 gem install lolcat
 figlet -f big Done !!! | lolcat
 echo
-echo -e "\e[1m Now Run \e[32mbash t-ban.sh\e[0m...!!!"
+echo -e "\e[1m Now Run \e[32mbash banner.sh\e[0m...!!!"
 echo
 echo -e "\e[1m\e[32m Developed by :\e[33m Sant kr(Admin of Strix Cyber Community)"
 echo
