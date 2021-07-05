dc459cc112cb36b7b0ef7faa6257f48fa0f05eb7
STRIX.D <70028787+Strix2109@users.noreply.github.com>
2021-07-05 20:38:40 +0530

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -1,21 +1,14 @@
-#!/bin/bash
-#
-#
-# Termux Banner
-# Banner by Sant kr.
-#
-#
-clear
+
 figlet -f big Termux Banner | lolcat
 cyan='\e[0;36m'
 lightgreen='\e[1;32m'
 red='\e[1;31m'
 yellow='\e[1;33m'
-echo -e $lightgreen "\e[1m                Security Help For Ethical Hackers... "
+echo -e $lightgreen "\e[1m                Evil Eye Termux Banner.. "
 echo " "
-echo -e $yellow "\e[1m                         -Sant kr(Admin of Strix Cyber Community) "
+echo -e $yellow "\e[1m                         -Sant kr.(Admin of Strix Cyber Community) "
 echo " "
-echo " "
+echo -e $red "\e[1m                       Subscribe us on YT- STRIX.D (Search on YT) "
 echo " "
 echo -e "\e[1m\e[33m\nWhat is Your \e[31mBanner \e[33mName\e[32m :\n\n"
 read varbanner
@@ -23,10 +16,10 @@ echo
 echo -e "\e[1m\e[33m\nWhat is Your Cowsay Name\e[32m :\n\n "
 read varcowsay
 echo  "cowsay -f eyes "$varcowsay" | lolcat" > C0w54y.txt
-echo "toilet -f big '    $varbanner' -F gay | lolcat" > 84nn3r.txt
+echo "toilet -f big ' $varbanner' -F gay | lolcat" > 84nn3r.txt
 echo
 echo "clear" > cl34r.txt
-cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/zshrc
-cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 ls
-cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
